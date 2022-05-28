5b9b524b8c56ee6a72deab446a3bcb164d0268ab
BhautikPethani <c0854487@mylambton.ca>
2022-05-27 21:34:23 -0400

    Launch Screen Done

---
+++ FA_BhautikPethani_c0854487_iOS/Controller/SplashViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SplashViewController.swift
+//  FA_BhautikPethani_c0854487_iOS
+//
+//  Created by Bhautik Pethani on 2022-05-27.
+//
+
+import UIKit
+
+class SplashViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
