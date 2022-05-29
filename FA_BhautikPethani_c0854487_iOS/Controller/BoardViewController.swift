a6cc2551d7dcb914f3034917fda66f18c3e17c71
BhautikPethani <c0854487@mylambton.ca>
2022-05-29 00:56:50 -0400

    Color of the board highlighted once user gets win

--- FA_BhautikPethani_c0854487_iOS/Controller/BoardViewController.swift
+++ FA_BhautikPethani_c0854487_iOS/Controller/BoardViewController.swift
@@ -14,8 +14,14 @@ class BoardViewController: UIViewController {
     
     @IBOutlet var boardBox: [UIButton]!
     
-    var player1 = true
-    var player2 = false
+    var player1 = false
+    var player2 = true
+    
+    var player1WinColor = UIColor(red: 1.000, green: 0.228, blue: 0.599, alpha: 1.0)
+    var player2WinColor = UIColor(red: 0.078, green: 0.067, blue: 1.000, alpha: 0.7)
+    
+    var boardColor1 = UIColor(red: 0.875, green: 0.800, blue: 0.902, alpha: 1.0)
+    var boardColor2 = UIColor(red: 0.706, green: 0.753, blue: 0.961, alpha: 1.0)
     
     var board = [[0,0,0],[0,0,0],[0,0,0]]
     var counter = 1
@@ -23,17 +29,20 @@ class BoardViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        turnChangeBackgroundColor()
+        turnChange()
+        resetBoard()
         // Do any additional setup after loading the view.
     }
     
-    func turnChangeBackgroundColor(){
+    func turnChange(){
+        player1 = !player1
+        player2 = !player2
         if(counter % 2 == 1){
-            player1Img.backgroundColor = UIColor(red: 0.875, green: 0.800, blue: 0.902, alpha: 1.0);
-            player2Img.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.0);
+            player1Img.backgroundColor = boardColor1
+            player2Img.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.0)
         }else{
-            player2Img.backgroundColor = UIColor(red: 0.706, green: 0.753, blue: 0.961, alpha: 1.0);
-            player1Img.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.0);
+            player2Img.backgroundColor = boardColor2
+            player1Img.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.0)
         }
     }
     
@@ -72,12 +81,74 @@ class BoardViewController: UIViewController {
         
         (player1) ? sender.setBackgroundImage(UIImage(named: "cross"), for: .normal) : sender.setBackgroundImage(UIImage(named: "noughtSign"), for: .normal)
         
-        player1 = !player1
-        player2 = !player2
+        
+        if(checkResult()){
+            print ("Win");
+            disableAllButtons()
+        }
+        
         sender.isUserInteractionEnabled = false
         counter+=1
         
-        turnChangeBackgroundColor()
+        turnChange()
+    }
+    
+    func changeWinColor(button: [Int], boxColor: UIColor){
+        boardBox[button[0]].backgroundColor = boxColor;
+        boardBox[button[1]].backgroundColor = boxColor;
+        boardBox[button[2]].backgroundColor = boxColor;
+    }
+    
+    func checkResult() -> Bool{
+        if(board[0][0] == board[0][1] && board[0][1] == board[0][2] && board[0][0] != 0){
+            changeWinColor(button: [0,1,2], boxColor: (board[0][0] == 1) ? player1WinColor : player2WinColor)
+            return true
+        }
+        else if(board[1][0] == board[1][1] && board[1][1] == board[1][2] && board[1][0] != 0){
+            changeWinColor(button: [3,4,5], boxColor: (board[0][0] == 1) ? player1WinColor : player2WinColor)
+            return true
+        }
+        else if(board[2][0] == board[2][1] && board[2][1] == board[2][2] && board[2][0] != 0){
+            changeWinColor(button: [6,7,8], boxColor: (board[0][0] == 1) ? player1WinColor : player2WinColor)
+            return true
+        }
+        else if(board[0][0] == board[1][0] && board[1][0] == board[2][0] && board[0][0] != 0){
+            changeWinColor(button: [0,3,6], boxColor: (board[0][0] == 1) ? player1WinColor : player2WinColor)
+            return true
+        }
+        else if(board[0][1] == board[1][1] && board[1][1] == board[2][1] && board[0][1] != 0){
+            changeWinColor(button: [1,4,7], boxColor: (board[0][0] == 1) ? player1WinColor : player2WinColor)
+            return true
+        }
+        else if(board[0][2] == board[1][2] && board[1][2] == board[2][2] && board[0][2] != 0){
+            changeWinColor(button: [2,5,8], boxColor: (board[0][0] == 1) ? player1WinColor : player2WinColor)
+            return true
+        }
+        else if(board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != 0){
+            changeWinColor(button: [0,4,8], boxColor: (board[0][0] == 1) ? player1WinColor : player2WinColor)
+            return true
+        }
+        else if(board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] != 0){
+            changeWinColor(button: [2,4,6], boxColor: (board[0][0] == 1) ? player1WinColor : player2WinColor)
+            return true
+        }
+        return false
+    }
+    
+    func resetBoard(){
+        for i in 0...8{
+            if(i % 2 == 0){
+                boardBox[i].backgroundColor = boardColor1
+            }else{
+                boardBox[i].backgroundColor = boardColor2
+            }
+        }
+    }
+    
+    func disableAllButtons(){
+        for i in 0...8{
+            boardBox[i].isUserInteractionEnabled = false
+        }
     }
     
 }
